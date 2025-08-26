25535fe8a6a6948c56088b29892cafbc4d25fdba
DavidValenciaX <60826603+DavidValenciaX@users.noreply.github.com>
2025-08-25 19:13:33 -0500

    refactor: Initialize upcoming videos array to an empty state
    - Updated the upcoming variable in app.js to default to an empty array instead of a predefined list of videos.
    - This change allows for a cleaner state management approach, relying on localStorage for dynamic content retrieval.

--- app.js
+++ app.js
@@ -44,13 +44,7 @@ function ($window, $rootScope, $log, $timeout, $http, localStorageService) {
   };
 
   let searchResults = [];
-  let upcoming = localStorageService.get(STORAGE_KEYS.UPCOMING) || [
-    {id: 'kRJuY6ZDLPo', title: 'La Roux - In for the Kill (Twelves Remix)'},
-    {id: '45YSGFctLws', title: 'Shout Out Louds - Illusions'},
-    {id: 'ktoaj1IpTbw', title: 'CHVRCHES - Gun'},
-    {id: '8Zh0tY2NfLs', title: 'N.E.R.D. ft. Nelly Furtado - Hot N Fun (Boys Noize Remix) HQ'},
-    {id: 'zwJPcRtbzDk', title: 'Daft Punk - Human After All (SebastiAn Remix)'}
-  ];
+  let upcoming = localStorageService.get(STORAGE_KEYS.UPCOMING) || [];
 
 
 
